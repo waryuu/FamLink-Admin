627887e715953945027982ed8c0b9ae3e429498d
BerlySet <berlyswn22@gmail.com>
2022-01-07 16:53:48 +0700

    create event done

--- app/Http/Controllers/EventCT.php
+++ app/Http/Controllers/EventCT.php
@@ -8,6 +8,7 @@ use Yajra\DataTables\DataTables;
 use RealRashid\SweetAlert\Facades\Alert;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
 
 class EventCT extends Controller
 {
@@ -46,49 +47,40 @@ class EventCT extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
-        // $request->validate([
-        //     'title' => 'required',
-        //     'content' => 'required',
-        //     'status' => 'required',
-        //     'image' => 'required|mimes:jpeg,jpg,png,gif|required|max:1000',
-        //     ]
-        // );
+        $request->validate([
+            'image' => 'required|mimes:jpeg,jpg,png,gif|required|max:1000',
+            'title' => 'required',
+            'organizer' => 'required',
+            'price' => 'required',
+            'time' => 'required',
+            'location' => 'required',
+            'description' => 'required',
+            'registlink' => 'required',
+            'status' => 'required',
+            ]
+        );
+        
+        $user = Auth::user();
+        $model = new EventModel();
+        $model->title = $request->title;
+        $model->id_staff = $user->id;
+        $model->organizer = $request->organizer;
+        $model->price = $request->price;
+        $model->time = $request->time;
+        $model->location = $request->location;
+        $model->description = $request->description;
+        $model->registlink = $request->registlink;
+        $model->status = $request->status;
+        $model->created_at = Carbon::now();
+        $model->save();
 
-        // $model = new ArticleModel();
-        // $model->title = $request->title;
-        // $model->content = $request->content;
-        // $model->status = $request->status;
-        // $model->type = $request->type;
-        // $model->created_at = Carbon::now();
-        // $model->save();
+        $fileName = $model->id.'-'.time().'.'.$request->image->extension();
+        $request->image->move(public_path('event'), $fileName);
+        $model->image = $fileName;
+        $model->save();
 
-        // $fileName = $model->id.'-'.time().'.'.$request->image->extension();
-        // $request->image->move(public_path('article'), $fileName);
-        // $model->image = $fileName;
-        // $model->save();
-
-        // if (isset($request->type_value)) {
-        //     $fileName = $model->id.'-'.time().'.'.$request->type_value->extension();
-        //     $request->type_value->move(public_path('article_pdf'), $fileName);
-        //     $model->type_value = $fileName;
-        //     $model->save();
-        // }
-
-        // $fcm_data['to'] = "/topics/GLOBAL";
-
-        // $data['title'] = 'Artikel Terbaru Famlink';
-        // $data['body'] = $model->title;
-        // $fcm_data['data'] = $data;
-
-        // $response = Http::withHeaders([
-        //     'Authorization' => 'key=AAAAbuzphk8:APA91bHu2-MEMfW1UlZwLQRjczUhGQRy9Vuse8un-DJTpW7M5_igZ-L9GpXXU3OV_7AVjbZ9coRTtjpIeXNqUlDhoz0sC5jbV3j5e3urlclhDtDtBQ2DDybYCNHdmR5QRm-7RHFJMB_Y',
-        //     'Content-Type' => 'application/json'
-        // ])->post('https://fcm.googleapis.com/fcm/send', $fcm_data);
-
-
-        // Alert::success('Berhasil', 'Anda berhasil menginputkan data');
-        // return redirect()->to('/admin/article');
+        Alert::success('Berhasil', 'Anda berhasil menginputkan data');
+        return redirect()->to('/admin/event');
     }
 
     /**
